559c3ec805c5722737a249433cf6462d2c364925
Harrison Hesslink <harrisonhesslink24@gmail.com>
2020-10-31 12:24:22 -0700

    Relay

--- network/inbound.go
+++ network/inbound.go
@@ -250,7 +250,9 @@ func (s *Server) HandleData(ctx *flatend.Context, request []byte) {
 
 	if !s.inMempool(payload.Oracle_Data.Hash) {
 		s.Prtl.Mempool.Transactions = append(s.Prtl.Mempool.Transactions, payload.Oracle_Data)
+		go s.BroadCastOracleData(paylaod.Oracle_Data)
 	}
+
 	log.Println(util.Rcv + " [" + command + "] Oracle Data: " + payload.Oracle_Data.Hash)
 }
 
