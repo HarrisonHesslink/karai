896f8d45211599cacd97ada6f0d82a794cce4494
Harrison Hesslink <harrisonhesslink24@gmail.com>
2020-12-01 10:27:34 -0800

    fix creating table

--- database/db.go
+++ database/db.go
@@ -60,7 +60,7 @@ func (d *Database) CreateTables() {
 
 	tx := db.MustBegin()
 
-	q := "CREATE TABLE IF NOT EXISTS " + d.Cf.GetTableName() + "(tx_time CHAR(19) NOT NULL, tx_type CHAR(1) NOT NULL, tx_hash CHAR(128) NOT NULL, tx_data TEXT NOT NULL, tx_prev CHAR(128) NOT NULL, tx_epoc TEXT NOT NULL, tx_subg CHAR(128) NOT NULL, tx_prnt CHAR(128), tx_mile BOOLEAN NOT NULL,tx_lead BOOLEAN NOT NULL, tx_height bigint);"
+	q := "CREATE TABLE IF NOT EXISTS " + d.Cf.GetTableName() + "(tx_time CHAR(19) NOT NULL, tx_type CHAR(1) NOT NULL, tx_hash CHAR(128) NOT NULL, tx_data TEXT NOT NULL, tx_prev CHAR(128) NOT NULL, tx_epoc TEXT NOT NULL, tx_subg CHAR(128) NOT NULL, tx_prnt CHAR(128), tx_mile BOOLEAN NOT NULL,tx_lead BOOLEAN NOT NULL, tx_height BIGINT);"
 	tx.MustExec(q)
 	tx.Commit()
 }
