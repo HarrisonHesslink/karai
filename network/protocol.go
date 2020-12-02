07aa62e2a89655c1d16f87896c2071602886da8e
Harrison Hesslink <harrisonhesslink24@gmail.com>
2020-12-02 01:47:55 -0800

    go back to 10th block gov change

--- network/protocol.go
+++ network/protocol.go
@@ -213,10 +213,9 @@ func (s *Server) NewConsensusTXFromCore(req transaction.NewBlock) {
 	req_string, _ := json.Marshal(req)
 
 	height := req.Height
-	log.Println(height)
-	// if height%10 != 0 {
-	// 	return
-	// }
+	if height%10 != 0 {
+		return
+	}
 
 	go s.Prtl.Mempool.PruneHeight(height - 5)
 
